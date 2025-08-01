5f8ccdd89ac62bde8a45b8947433af1f2a363bb7
Gokul G <gokulgovindaraj08@gmail.com>
2025-08-01 08:42:28 +0530

    some new concepts

--- Learn.java
+++ Learn.java
@@ -1,13 +1,52 @@
 
-public class Learn {
-    public static void main(String arr[]) {
-        Calculate obj = new Calculate();
-       System.out.println(obj.add(10,20));
-    }
-}
-class Calculate{
-    public int add(int a,int b)
-    {
-        return a+b;
+// CLASS AND OBJECT
+
+
+// public class Learn {
+//     public static void main(String arr[]) {
+//         Calculate obj = new Calculate();
+//        System.out.println(obj.add(10,20));
+//     }
+// }
+// class Calculate{
+//     public int add(int a,int b)
+//     {
+//         return a+b;
+//     }
+// }
+
+
+//###########################################
+
+//    STATIC EXAMPLE
+// class Basic{
+//     static int nums;
+//     public Basic(int nums)
+//     {
+//         this.nums=nums;
+//     }
+//     public int fun()
+//     {
+//         return  nums;
+//     }
+// }
+// public class Learn{
+//     public static void main(String[] args) {
+//         Basic obj = new Basic(5);
+//         Basic obj1 = new Basic(5);
+//         Basic.nums=10;
+//         System.out.println(obj.fun()+" " + obj1.fun());
+//     }
+// }
+
+//###########################################//
+
+
+// STRING EXAMPLE
+
+public class Learn{
+    public static void main(String[] args) {
+        String s1 = new String("Gokul");
+        System.out.println(s1);
     }
 }
