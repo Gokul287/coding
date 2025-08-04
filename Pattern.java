3cb909fe133ae8c7cd706cef447733b8f62722c9
Gokul G <gokulgovindaraj08@gmail.com>
2025-08-04 13:16:21 +0530

    until 17 th tuf pattern

--- Pattern.java
+++ Pattern.java
@@ -8,7 +8,7 @@ public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        diamondPattern(n);
+        characterReversePyramidPattern(n);
         System.out.println("Pattern Printing");
     }
 
@@ -194,6 +194,193 @@ public class Pattern {
     //         }
     //         System.out.println();
     //     }
+    // }
+
+    //################################################
+
+
+    // HALF DIAMOND PATTERN
+
+    //  public static void halfDiamondPattern(int n)
+    // {
+    //     int t=n;
+    //     for(int i=0;i<n*2;i++)
+    //     {
+    //        if(i<n)
+    //        {
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print("*");
+    //         }
+    //        }
+    //        else{
+    //             t-=1;
+    //             for(int j=t;j>0;j--)
+    //             {
+    //                 System.out.print("*");
+    //             }
+    //        }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    //################################################
+
+
+    // TUF 11TH PATTERN 0 AND 1 PATTERN
+
+    // public static void zeroOnePattern(int n)
+    // {
+    //     int c=1;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         c=(i%2==0)?1:0;
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //            System.out.print(c+" ");
+    //            c=(c==1)?0:1;
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+     //################################################
+
+     // TUF 12TH PROBLEM (V GAP PATTERN)
+
+    //  public static void vGapPattern(int n)
+    //  {
+    //     int g=2;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(j+1);
+    //         }
+
+    //         for(int s=0;s<(n*2)-g;s++)
+    //         {
+    //             System.out.print(" ");
+    //         }
+    //         g+=2;
+
+    //         for(int j=i+1;j>0;j--)
+    //         {
+    //             System.out.print(j);
+    //         }
+    //         System.out.println();
+    //     }
+    //  }
+
+
+     //################################################
+
+
+    // TRIANGLE NUMBER PATTERN 3 (TUF 13TH PROBLEM)
+    //  public static void trianglePattern3(int n)
+    // {
+    //     int c=1;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(c++ +" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+     //################################################
+
+
+    // TRIANGLE CHARACTER PATTERN (TUF 14TH PROBLEM)
+    //  public static void triangleCharacterPattern(int n)
+    // {
+    //     char c='A';
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         c='A';
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(c++ +" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    //################################################
+
+
+    // INVERTED TRIANGLE CHARACTER PATTERN (TUF 15TH PROBLEM)
+    //  public static void invertedTriangleCharacterPattern(int n)
+    // {
+    //     char c='A';
+    //     for(int i=n;i>0;i--)
+    //     {
+    //         c='A';
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(c++ +" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    //################################################
+
+
+    // TRIANGLE CHARACTER PATTERN2 (TUF 16TH PROBLEM)
+    //  public static void triangleCharacterPattern2(int n)
+    // {
+    //     char c='A';
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(c +" ");
+    //         }
+    //         c+=1;
+    //         System.out.println();
+    //     }
+    // }
+
+
+    //################################################
+
+
+    //  CHARACTER REVERSE PYRAMID PATTERN (TUF 17TH PROBLEM)
+    //  public static void characterReversePyramidPattern(int n)
+    // {
+    //     char c='A';
+    //     int v=0;
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         for(int s=0;s<n-i;s++)
+    //         {
+    //             System.out.print(" ");
+    //         }
+
+    //         for(int j=1;j<=(i*2)-1;j++)
+    //         {
+    //             if(j<=i)
+    //             {
+    //                 c=(char)('A'+ j-1);
+    //             }
+    //             else{
+    //                 c=(char)('A'+ i*2-j-1);
+    //             }
+    //             System.out.print(c);
+    //         }
+            
+    //         System.out.println();
+    //     }
+            
+        
     // }
 
 }
