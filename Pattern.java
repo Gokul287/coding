133e913b5b5502fe83ac4dbf9e9199990c3e0ac3
Gokul G <gokulgovindaraj08@gmail.com>
2025-08-04 11:32:49 +0530

    more Pattern

--- Pattern.java
+++ Pattern.java
@@ -3,6 +3,7 @@ public class Pattern{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
+        invertedTrianglePattern2(n);
         System.out.println("Pattern Printing");
     }
     
@@ -19,7 +20,111 @@ public class Pattern{
         //     }
         // }
 
-        
+    //################################################
+    
+    
+    // RIGHT ANGLE TRIANGLE PATTERN
+    // public static void trianglePattern(int n)
+    // {
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(" * ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    //################################################
+
+    // TRIANGLE NUMBER PATTERN 1
+
+    //  public static void trianglePattern1(int n)
+    // {
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(i+1+" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+     //################################################
+
+    // TRIANGLE NUMBER PATTERN 2
+
+    //  public static void trianglePattern2(int n)
+    // {
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<i+1;j++)
+    //         {
+    //             System.out.print(j+1+" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+
+    //################################################
+
+    // INVERTED TRIANGLE PATTERN 
+
+    //  public static void invertedTrianglePattern(int n)
+    // {
+    //     for(int i=n;i>0;i--)
+    //     {
+    //         for(int j=0;j<i;j++)
+    //         {
+    //             System.out.print(" * ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    //################################################
+
+    // INVERTED TRIANGLE NUMBER PATTERN 1
+
+    //  public static void invertedTrianglePattern1(int n)
+    // {
+    //     for(int i=n;i>0;i--)
+    //     {
+    //         for(int j=0;j<i;j++)
+    //         {
+    //             System.out.print(j+1+" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+
+    //################################################
+
+    // INVERTED TRIANGLE NUMBER PATTERN 2
+
+    //  public static void invertedTrianglePattern2(int n)
+    // {
+    //     for(int i=n;i>0;i--)
+    //     {
+    //         for(int j=0;j<i;j++)
+    //         {
+    //             System.out.print(i+" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+
 
 }
 
