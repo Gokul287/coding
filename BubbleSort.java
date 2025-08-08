91aa1f22dd33df8275d29fa9c679fe79fce4b0d9
Gokul G <gokulgovindaraj08@gmail.com>
2025-08-08 12:25:54 +0530

    BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=5;
+        int arr[] =  new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        bubbleSort(arr, n);
+        System.out.println("Sorted array: Usong Bubble Sort");
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void bubbleSort(int arr[], int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n-i-1;j++)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp= arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+}
