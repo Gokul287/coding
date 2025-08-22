ab3657d6a4315e1ff75dda332f66f34cd6ac61b5
Gokul G <gokulgovindaraj08@gmail.com>
2025-08-22 14:42:48 +0530

    Update

--- LinkedList/DoublyLinkedlist.java
+++ LinkedList/DoublyLinkedlist.java
@@ -6,6 +6,9 @@ public class DoublyLinkedList {
       list.addNode(20);
       list.addNode(30);
       list.display();
+      list.insertAtBeginning(5);
+      list.deleteNode(20);
+      list.display();
    }
     // Source code is decompiled from a .class file using FernFlower decompiler.
 static class Node {
@@ -45,6 +48,48 @@ static class Node {
       }
    }
 
+   public void insertAtBeginning(int data) {
+      Node newNode = new Node(data);
+      if (this.head == null) {
+         this.head = newNode;
+         this.head.prev = null;
+         this.head.next = null;
+      } else {
+         newNode.next = this.head;
+         this.head.prev = newNode;
+         this.head = newNode;
+         this.head.prev = null;
+      }
+   }
+
+   public void deleteNode(int data) {
+      if (this.head == null) {
+         System.out.println("List is empty. Cannot delete " + data);
+         return;
+      }
+
+      Node current = this.head;
+      while (current != null && current.data != data) {
+         current = current.next;
+      }
+
+      if (current == null) {
+         System.out.println("Node with value " + data + " not found.");
+         return;
+      }
+
+      if (current.prev != null) {
+         current.prev.next = current.next;
+      } else {
+         this.head = current.next; // Update head if it's the first node
+      }
+
+      if (current.next != null) {
+         current.next.prev = current.prev;
+      }
+
+      System.out.println("Deleted node with value " + data);
+   }
    public void display() {
       Node current = this.head;
       if (current == null) {
