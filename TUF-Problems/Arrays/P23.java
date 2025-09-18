21852b47f5de7b1821769db9d37a633296f906b1
Gokul G <gokulgovindaraj08@gmail.com>
2025-09-18 20:37:50 +0530

    alter

--- TUF-Problems/Arrays/P23.java
+++ TUF-Problems/Arrays/P23.java
@@ -32,7 +32,7 @@ public class P23 {
         // Transpose of matrix
         for(int i=0;i<n;i++)
         {
-            for(int j=i;j<m;j++)
+            for(int j=i+1;j<m;j++)
             {
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
